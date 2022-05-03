dc96ae81c623082c47c3ee7812013e4a2e3bf919
johncalabro <j.calabro4@gmail.com>
2022-05-02 21:29:29 -0400

    commit tree aca9921f92028ad44d735104318bafe4adacc13e

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+
+        const findMatching = (name, match) => name.filter(
+              goldylox => goldylox.toLowerCase() === match.toLowerCase()
+            );
+          
+        const fuzzyMatch = (name2) => name2.filter(
+            bear => bear.startsWith('s')
+        );
+
+
+// const result = words.filter(word => word.length > 6);
+
+// findMatching- This function takes an array of drivers' 
+// names and a string as arguments, 
+// and returns the matching list of drivers. 
+// The function should be case insensitive.
+
+
+    // [1, 2, 3, 4, 5].filter(function (num) {
+    //     return num > 3;
+    //   });
+      
